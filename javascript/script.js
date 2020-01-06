5e924dc17f9174e1a60beade41c15616718ce7c7
RichardKrikler <krikler.richard.20@gmail.com>
2020-01-06 18:18:46 +0100

    Navigation is now fixed when the menu is open

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+let nav = document.getElementsByTagName("nav")[0];
 
 let menuBt = document.getElementById("menuBt");
 menuBt.addEventListener("click", showMenu);
@@ -7,8 +8,10 @@ let menuOverlay = document.getElementById("menuOverlay");
 
 function showMenu() {
     if (menu.classList == "") {
+        nav.className = "visible";
         menu.className = "visible animated  tdFadeIn";
     } else {
+        nav.className = "";
         menu.className = "visible animated  tdFadeOut";
         setTimeout(() => {
             menu.className = "";
