4c3875bacfdfff818a0c97cd06eb6226c7c240b4
Richard Krikler <8112@htl.rennweg.at>
2020-03-20 17:14:59 +0100

    added Blog link in the Menu Bar

--- javascript/germanMenu.js
+++ javascript/germanMenu.js
@@ -6,6 +6,7 @@ function reloadMenu() {
     for (let i = 0; i < menuOverlay.length; i++) {
         menuOverlay[i].innerHTML = "<li><a href=\"/\">Home</a></li>" +
             "<li><a href=\"/projects\">Projekte</a></li>" +
+            "<li><a href=\"/blog\">Blog</a></li>" +
             "<li><a href=\"/contact\">Kontakt</a></li>";
     }
 }
