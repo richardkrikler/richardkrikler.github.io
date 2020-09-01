5843f4af71e5e8a4def2361fb6c5721f03ca5a64
Richard Krikler <8112@htl.rennweg.at>
2020-09-01 21:39:18 +0200

    image viewer fix

--- javascript/script.js
+++ javascript/script.js
@@ -27,3 +27,17 @@ function showMenu() {
         }, 100);
     }
 }
+
+let images = document.getElementsByClassName("images");
+for (let i = 0; i < images.length; i++) {
+    let currentId = images[i].id;
+    let idElement = document.getElementById(currentId);
+    for (let j = 0; j < idElement.children.length; j++) {
+        idElement.children[j].addEventListener("click", function (e) {
+            BigPicture({
+                el: e.target,
+                gallery: '#' + currentId,
+            })
+        })
+    }
+}
