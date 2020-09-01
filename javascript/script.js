773400670d6b3e65c43e88538472f279ab849738
Richard Krikler <8112@htl.rennweg.at>
2020-09-01 16:31:08 +0200

    link fixes

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,16 @@ menuBt.addEventListener("click", showMenu);
 
 let menu = document.getElementById("menu");
 
+let menuOverlay = document.getElementsByClassName("menuOverlay");
+window.addEventListener("load", function () {
+    for (let i = 0; i < menuOverlay.length; i++) {
+        menuOverlay[i].innerHTML = "<li><a href=\"/\">Home</a></li>" +
+            "<li><a href=\"/projects\">Projects</a></li>" +
+            "<li><a href=\"/blog\">Blog</a></li>" +
+            "<li><a href=\"/contact\">Contact</a></li>";
+    }
+});
+
 function showMenu() {
     if (menu.classList == "") {
         nav.className = "visible";
